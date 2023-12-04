f131e6a3b99afc26949ee4c99b458ab8cf852ced
Shestakov Viktor <phd.shestakov@gmail.com>
2023-12-04 17:20:09 +0300

    chore(type): add types system based on jsdoc + vscode

--- src/app.js
+++ src/app.js
@@ -2,8 +2,15 @@ import "@src/i18n";
 import {initCore} from "@core";
 import {initUI} from "@ui";
 
+/**
+ * @typedef {Object} Context
+ * @property {number} width
+ * @property {number} hasCourage
+ */
+const context = {
+    width: 0,
+};
 
-const context = {};
 
 initCore(context);
 initUI(context);
