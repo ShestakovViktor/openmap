76807e650c9c5f7fa7efe6e66535fb0a65219a6f
Shestakov Viktor <phd.shestakov@gmail.com>
2024-06-03 17:31:00 +0300

    refactor(ui): move propSelect content to entity propSection

--- src/ui/entity/widget/PropSection/PropSection.tsx
+++ src/ui/entity/widget/PropSection/PropSection.tsx
@@ -1,20 +1,80 @@
 import en from "./string/en.json";
 import i18next from "i18next";
+import styles from "./PropSection.module.scss";
+import PaletteIconSvg from "@public/icon/palette.svg";
 
-import {Section} from "@ui/widget";
-import {JSX, Resource} from "solid-js";
-import {Id} from "@type";
-import {PropSelect} from "@ui/prop/widget";
+import {Button, Dialog, Modal, Section, Toolbar} from "@ui/widget";
+import {JSX, Resource, createMemo, createResource} from "solid-js";
+import {Id, Prop} from "@type";
+import {PropBrowser} from "@ui/prop/widget";
+import {useEditorContext} from "@ui/editor/context";
+import {assetToSrc} from "@ui/app/utiliy";
+import {DATA} from "@enum";
 
 i18next.addResourceBundle("en", "entity", {PropSection: en}, true, true);
 
+type Entity = {propId: Id | null};
+
 type Props = {
-    entity: Resource<{propId: Id | null} | undefined>;
+    entity: Resource<Entity | undefined>;
 };
 
 export function PropSection(props: Props): JSX.Element {
+    const editorCtx = useEditorContext();
+    let input: HTMLInputElement | undefined;
+
+    const [propId, {mutate: setPropId}]
+        = createResource<Id | null, Entity | undefined>(
+            () => props.entity(),
+            (entity) => entity ? entity.propId : null
+        );
+
+    const src = createMemo((): string => {
+        const id = propId();
+
+        if (!id) {
+            return "./icon/no.svg";
+        }
+        else {
+            const asset = editorCtx.store.asset
+                .getById<Prop>(id);
+
+            if (!asset) throw new Error();
+
+            return asset.path || assetToSrc(asset);
+        }
+    });
+
+    const selected = createMemo(() => {
+        const id = propId();
+        return id ? [id] : [];
+    });
+
+    const assetBrowserDialog = new Modal();
+    assetBrowserDialog.render(
+        <Dialog
+            class={styles.AssetBrowserDialog}
+            onClose={() => assetBrowserDialog.hide()}
+        >
+            <PropBrowser
+                selected={selected()}
+                onSelect={(ids: Id[]) => {
+                    if (input) {
+                        input.value = String(ids[0]);
+                        input.dispatchEvent(
+                            new Event("change", {bubbles: true})
+                        );
+                    }
+                    setPropId(ids[0]);
+                    assetBrowserDialog.hide();
+                }}
+            />
+        </Dialog>
+    );
+
     return (
         <Section
+            class={styles.PropSection}
             title={
                 i18next.t(
                     "entity:PropSection.title",
@@ -22,7 +82,25 @@ export function PropSection(props: Props): JSX.Element {
                 )
             }
         >
-            <PropSelect entity={props.entity}/>
+            <Toolbar>
+                <Button
+                    icon={PaletteIconSvg}
+                    onClick={() => {
+                        assetBrowserDialog.show();
+                    }}
+                />
+            </Toolbar>
+            <input
+                ref={input}
+                name={"propId"}
+                type={"hidden"}
+                value={propId() ?? ""}
+                data-type={DATA.REFERENCE}
+            />
+            <img
+                class={styles.Preview}
+                src={src()}
+            />
         </Section>
     );
 }
--- src/ui/prop/widget/index.ts
+++ src/ui/prop/widget/index.ts
@@ -1,3 +1,2 @@
 export * from "./PropForm";
 export * from "./PropBrowser";
-export * from "./PropSelect";
